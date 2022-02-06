9f102f4cf4a0f104873f1276609668450caa2469
Gibert Quentin <quentingibert6@gmail.com>
2022-02-06 11:14:38 +0100

    finalisation

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const btnOpen = document.querySelector(".btn-open");
 const nav = document.querySelector(".nav");
 const btnClose = document.querySelector(".btn-close");
+const btnProject = document.querySelector("#projet");
+const modal = document.querySelector(".modal");
+const closeModalBtn = document.querySelector(".btn-project");
+
+closeModalBtn.addEventListener("click", () => {
+  modal.classList.remove("showModal");
+});
+
+btnProject.addEventListener("click", () => {
+  modal.classList.add("showModal");
+});
 
 btnOpen.addEventListener("click", () => {
   nav.classList.toggle("show-nav");
@@ -14,5 +25,5 @@ const loading = document.querySelector(".loading");
 window.addEventListener("load", () => {
   setTimeout(() => {
     loading.classList.add("hideLoader");
-  }, 12000);
+  });
 });
